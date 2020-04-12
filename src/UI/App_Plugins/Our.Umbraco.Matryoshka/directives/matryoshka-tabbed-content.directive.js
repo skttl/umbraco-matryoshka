5471f4e1f3cc80bb69e7f59c7024ef5496bb68bd
Søren Kottal <sk@ecreo.dk>
2020-04-12 16:19:58 +0200

    Hide overflowing tabs (#12)

--- src/UI/App_Plugins/Our.Umbraco.Matryoshka/directives/matryoshka-tabbed-content.directive.js
+++ src/UI/App_Plugins/Our.Umbraco.Matryoshka/directives/matryoshka-tabbed-content.directive.js
@@ -10,6 +10,56 @@
             if ($scope.content.tabs[0]) {
                 $scope.currentTab = $scope.content.tabs[0].label;
             }
+
+            $scope.overflowingTabs = 0;
+
+
+            var tabNavItemsWidths = [];
+            // the parent is the component itself so we need to go one level higher
+            var container = $element.parent().parent();
+
+            $timeout(function(){
+                $element.find(".matryoshka-tabs-list li:not(.umb-tab--expand)").each(function() {
+                    tabNavItemsWidths.push($(this).outerWidth());
+                });
+                calculateWidth();
+            });
+
+            function calculateWidth(){
+                $timeout(function(){
+                    // 70 is the width of the expand menu (three dots) + 20 for the margin on umb-tabs-nav
+                    var containerWidth = container.width() - 90;
+                    var tabsWidth = 0;
+                    $scope.overflowingSections = 0;
+                    $scope.needTray = false;
+                    $scope.maxTabs = tabNavItemsWidths.length;
+
+                    // detect how many tabs we can show on the screen
+                    for (var i = 0; i <= tabNavItemsWidths.length; i++) {
+                        
+                        var tabWidth = tabNavItemsWidths[i];
+                        tabsWidth += tabWidth;
+
+                        if(tabsWidth >= containerWidth) {
+                            $scope.needTray = true;
+                            $scope.maxTabs = i;
+                            $scope.overflowingTabs = $scope.maxTabs - $scope.content.tabs.length;
+                            break;
+                        }
+                    }
+                    
+                });
+            }
+
+            $(window).on('resize.tabsNav', function () {
+                console.log("resize");
+                calculateWidth();
+            });
+
+            $scope.$on('$destroy', function() {
+                $(window).off('resize.tabsNav');
+            });
+
         }
 
         function controller($scope, $element, $attrs) {
@@ -53,6 +103,21 @@
                     }
                 }
             );
+                
+            $scope.needTray = false;
+            $scope.showTray = false;
+            $scope.overflowingSections = 0;
+
+            $scope.toggleTray = toggleTray;
+            $scope.hideTray = hideTray;
+            
+            function toggleTray() {
+                $scope.showTray = !$scope.showTray;
+            }
+
+            function hideTray() {
+                $scope.showTray = false;
+            }
         }
 
         var directive = {
