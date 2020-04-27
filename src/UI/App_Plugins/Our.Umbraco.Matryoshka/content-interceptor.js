b4866c8bc6486586691c4809ec9867de9fa28ae0
Søren Kottal <snielsen43@gmail.com>
2020-04-27 16:57:43 +0200

    Add support for member app

--- src/UI/App_Plugins/Our.Umbraco.Matryoshka/content-interceptor.js
+++ src/UI/App_Plugins/Our.Umbraco.Matryoshka/content-interceptor.js
@@ -10,6 +10,10 @@
                         response.data = response.data.replace("umb-tabbed-content", "matryoshka-tabbed-content");
                     }
 
+                    else if (response.config.url.includes("views/member/apps/content/content.html")) {
+                        response.data = "<div class=\"form-horizontal\" ng-controller=\"Umbraco.Editors.Member.Apps.ContentController as vm\"><matryoshka-tabbed-content ng-if=\"!loading\" content=\"content\"></matryoshka-tabbed-content></div>";
+                    }
+
                     return response;
                 }
             };
